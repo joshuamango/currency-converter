f06bc3d108d4f3bfe116c5b3b7f901face9bc2f2
joshuamango <joshster13@gmail.com>
2019-04-11 17:31:53 -0400

    Fix graph and add color palatte

--- src/index.js
+++ src/index.js
@@ -23,6 +23,7 @@ class App extends React.Component {
       to: "EUR",
       fromSymbol: "$",
       toSymbol: "€",
+      theme: "dark",
       data: {
         labels: [],
         datasets: [
@@ -43,6 +44,7 @@ class App extends React.Component {
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleChoiceFrom = this.handleChoiceFrom.bind(this);
     this.handleGraph = this.handleGraph.bind(this);
+    this.handleTheme = this.handleTheme.bind(this);
   }
 
   render() {
@@ -60,21 +62,31 @@ class App extends React.Component {
         position: "bottom"
       },
       scales: {
-        yAxes: [{
-          scaleLabel: {
-            display: true,
-            labelString: `${this.state.from} per 1 ${this.state.to}`
+        yAxes: [
+          {
+            scaleLabel: {
+              display: true,
+              labelString: `${this.state.to} per 1 ${this.state.from}`
+            }
           }
-        }]
+        ]
       }
     };
-  
 
     // This return statement contains everything that is displayed on the page.
     return (
       <Container className="App">
-        <Navbar bg="dark" variant="dark" id="navbar">
+        <Navbar bg={this.state.theme} variant="dark" id="navbar">
           <Navbar.Brand id="title">Currency Converter</Navbar.Brand>
+          <a
+            id="palette"
+            href="#"
+            onClick={this.handleTheme}
+            style={{ marginRight: "10px", paddingTop: "10px" }}
+            variant="outline-light"
+          >
+            <ion-icon id="palette" name="color-palette" />
+          </a>
           <Button
             variant="outline-light"
             href="https://www.github.com/joshuamango/currency-converter/"
@@ -88,7 +100,7 @@ class App extends React.Component {
             <div id="input-group-1">
               <InputGroup>
                 <DropdownButton
-                  variant="outline-secondary"
+                  variant={this.state.theme}
                   title={this.state.from}
                   onSelect={this.handleChoiceTo}
                   id="left-dropdown"
@@ -96,14 +108,18 @@ class App extends React.Component {
                 >
                   <Dropdown.Item eventKey="1">US Dollar (USD)</Dropdown.Item>
                   <Dropdown.Item eventKey="2">Euro (EUR)</Dropdown.Item>
-                  <Dropdown.Item eventKey="3">Pound (GBP)</Dropdown.Item>
-                  <Dropdown.Item eventKey="4">Yen (JPY)</Dropdown.Item>
+                  <Dropdown.Item eventKey="3">
+                    British Pound (GBP)
+                  </Dropdown.Item>
+                  <Dropdown.Item eventKey="4">Japanese Yen (JPY)</Dropdown.Item>
                   <Dropdown.Item eventKey="5">
                     Canadian Dollar (CAD)
                   </Dropdown.Item>
                   <Dropdown.Item eventKey="6">
                     Australian Dollar (AUD)
                   </Dropdown.Item>
+                  <Dropdown.Item eventKey="7">Swiss Franc (CHF)</Dropdown.Item>
+                  <Dropdown.Item eventKey="8">Chinese Yuan (CNY)</Dropdown.Item>
                 </DropdownButton>
                 <FormControl
                   id="input-1"
@@ -119,21 +135,25 @@ class App extends React.Component {
               <InputGroup>
                 <DropdownButton
                   as={InputGroup.Prepend}
-                  variant="outline-secondary"
+                  variant={this.state.theme}
                   title={this.state.to}
                   onSelect={this.handleChoiceFrom}
                   id="right-dropdown"
                 >
                   <Dropdown.Item eventKey="1">US Dollar (USD)</Dropdown.Item>
                   <Dropdown.Item eventKey="2">Euro (EUR)</Dropdown.Item>
-                  <Dropdown.Item eventKey="3">Pound (GBP)</Dropdown.Item>
-                  <Dropdown.Item eventKey="4">Yen (JPY)</Dropdown.Item>
+                  <Dropdown.Item eventKey="3">
+                    British Pound (GBP)
+                  </Dropdown.Item>
+                  <Dropdown.Item eventKey="4">Japanese Yen (JPY)</Dropdown.Item>
                   <Dropdown.Item eventKey="5">
                     Canadian Dollar (CAD)
                   </Dropdown.Item>
                   <Dropdown.Item eventKey="6">
                     Australian Dollar (AUD)
                   </Dropdown.Item>
+                  <Dropdown.Item eventKey="7">Swiss Franc (CHF)</Dropdown.Item>
+                  <Dropdown.Item eventKey="8">Chinese Yuan (CNY)</Dropdown.Item>
                 </DropdownButton>
                 <FormControl
                   id="input-2"
@@ -152,12 +172,68 @@ class App extends React.Component {
     );
   }
 
+  componentDidMount() {
+    this.handleGraph();
+  }
+
+  // Selects a random theme
+  handleTheme(e) {
+    let themeList = [
+      "dark",
+      "info",
+      "primary",
+      "secondary",
+      "danger",
+      "success",
+      "warning"
+    ];
+
+    let themeRGB = [
+      "rgba(52,58,64,0.3)", //dark
+      "rgba(23,162,184,0.3)", //info
+      "rgba(0,123,255,0.3)", //primary
+      "rgba(134,142,150,0.3)", //secondary
+      "rgba(220,53,69, 0.3)", //danger
+      "rgba(40,167,69,0.3)", //success
+      "rgba(255,193,7,0.3)" //warning
+    ];
+
+    let themeRGBDarker = [
+      "rgba(52,58,64,0.6)", //dark
+      "rgba(23,162,184,0.6)", //info
+      "rgba(0,123,255,0.6)", //primary
+      "rgba(134,142,150,0.6)", //secondary
+      "rgba(220,53,69, 0.6)", //danger
+      "rgba(40,167,69,0.6)", //success
+      "rgba(255,193,7,0.6)"
+    ];
+
+    let index = this.getRandomInt(0, 7);
+
+    let newThemeRGB = this.state.data;
+    newThemeRGB.datasets[0].backgroundColor = themeRGB[index];
+    newThemeRGB.datasets[0].borderColor = themeRGBDarker[index];
+
+    let newTheme = themeList[index];
+    this.setState({ theme: newTheme, data: newThemeRGB }, () => {
+      console.log(this.state.data.backgroundColor);
+    });
+  }
+
+  // Generates random number between two values
+  // Credit: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+  getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+  }
+
   /* Invoked when left dropdown selection changes. 
      Changes state to include new currency abbreviation
      and it associated symbol. */
   handleChoiceTo(key, evt) {
-    let abbr = ["USD", "EUR", "GBP", "JPY", "CAD", "AUD"];
-    let symbols = ["$", "€", "£", "¥", "$", "$"];
+    let abbr = ["USD", "EUR", "GBP", "JPY", "CAD", "AUD", "CHF", "CNY"];
+    let symbols = ["$", "€", "£", "¥", "$", "$", "₣", "元"];
     this.setState(
       {
         from: abbr[key - 1],
@@ -174,8 +250,8 @@ class App extends React.Component {
      Changes state to include new currency abbreviation
      and it associated symbol. */
   handleChoiceFrom(key, evt) {
-    let abbr = ["USD", "EUR", "GBP", "JPY", "CAD", "AUD"];
-    let symbols = ["$", "€", "£", "¥", "$", "$"];
+    let abbr = ["USD", "EUR", "GBP", "JPY", "CAD", "AUD", "CHF", "CNY"];
+    let symbols = ["$", "€", "£", "¥", "$", "$", "₣", "元"];
     this.setState(
       {
         to: abbr[key - 1],
@@ -189,64 +265,74 @@ class App extends React.Component {
   }
 
   handleGraph() {
-    const date = new Date();
-    const weekAgo = new Date();
-    weekAgo.setDate(date.getDate() - 7);
-    const year = date.getFullYear();
-    const day = date.getDate();
-    const month = "0" + (date.getMonth() + 1);
-    const url = `https://api.exchangeratesapi.io/history?start_at=${weekAgo.getFullYear()}-${"0" +
-      (weekAgo.getMonth() +
-        1)}-${weekAgo.getDate()}&end_at=${year}-${month}-${day}&symbols=${
-      this.state.from
-    },${this.state.to}&base=${this.state.from}`;
+    if (this.state.to === this.state.from) {
+      let currentState = { ...this.state.data };
+      let number = currentState.datasets[0].label.length;
+      let newArray = [];
+      while (number > 0) {
+        newArray.push(1);
+        number = number - 1;
+      }
+      currentState.datasets[0].data = newArray;
+      this.setState({ data: currentState });
+    } else {
+      const date = new Date();
+      const weekAgo = new Date();
+      weekAgo.setDate(date.getDate() - 7);
+      const year = date.getFullYear();
+      const day = date.getDate();
+      const month = "0" + (date.getMonth() + 1);
+      const url = `https://api.exchangeratesapi.io/history?start_at=${weekAgo.getFullYear()}-${"0" +
+        (weekAgo.getMonth() +
+          1)}-${weekAgo.getDate()}&end_at=${year}-${month}-${day}&symbols=${
+        this.state.from
+      },${this.state.to}&base=${this.state.from}`;
 
-    let newValues = [];
-    let newKeys = [];
-    let to = this.state.to
+      let newValues = [];
+      let newKeys = [];
+      let to = this.state.to;
 
-    fetch(url)
-      .then(resp => resp.json())
-      .then(function(data) {
-        let keys = Object.keys(data.rates);
-        let values = Object.values(data.rates);
-        let map = new Map();
-        for (let i = 0; i < keys.length; i++) {
-          map.set(keys[i], values[i][to]);
-        }
-
-        map = new Map([...map.entries()].sort());
-        let keyIterator = map.keys();
-        let valueIterator = map.values();
-
-        while (true) {
-          let element = keyIterator.next();
-          if (element.done === true) {
-            break;
-          } else {
-            newKeys.push(element.value);
+      fetch(url)
+        .then(resp => resp.json())
+        .then(data => {
+          let keys = Object.keys(data.rates);
+          let values = Object.values(data.rates);
+          let map = new Map();
+          for (let i = 0; i < keys.length; i++) {
+            map.set(keys[i], values[i][to]);
           }
-        }
 
-        while (true) {
-          let element = valueIterator.next();
-          if (element.done === true) {
-            break;
-          } else {
-            newValues.push(element.value);
+          map = new Map([...map.entries()].sort());
+          let keyIterator = map.keys();
+          let valueIterator = map.values();
+
+          while (true) {
+            let element = keyIterator.next();
+            if (element.done === true) {
+              break;
+            } else {
+              newKeys.push(element.value);
+            }
           }
-        }
-      })
-      .catch(function(error) {
-        console.log(error);
-      });
 
-    
-    let currentState = { ...this.state };
-    currentState.data.labels = newKeys;
-    currentState.data.datasets[0].data = newValues
-    
-    console.log(currentState);
+          while (true) {
+            let element = valueIterator.next();
+            if (element.done === true) {
+              break;
+            } else {
+              newValues.push(element.value);
+            }
+          }
+
+          let currentState = { ...this.state.data };
+          currentState.labels = newKeys;
+          currentState.datasets[0].data = newValues;
+          this.setState({ data: currentState });
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    }
   }
 
   // Invoked whenever the text in the left text-field is changed.
